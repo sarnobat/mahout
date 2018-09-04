42265295f3f5be27e73c045d95b283cca1268305
Sridhar Sarnobat <ss401533@gmail.com>
2018-09-03 22:10:12 -0700

    Minor changes (untested)

--- mahout-tfidf/src/main/java/com/technobium/MahoutTermClusterMwkSnptPointless.java
+++ mahout-tfidf/src/main/java/com/technobium/MahoutTermClusterMwkSnptPointless.java
@@ -51,7 +51,7 @@ import org.apache.mahout.vectorizer.tfidf.TFIDFConverter;
  * I'm not sure why we're trying to cluster the mwk files (as opposed to mwk
  * snippets). Each mwk file is suppsoed to be different.
  */
-public class MahoutTermClusterMwkSnpt {
+public class MahoutTermClusterMwkSnptPointless {
 
     public static void main(String args[]) throws Exception {
 
