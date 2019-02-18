c35762ecf8a787c8f1eab16dfec1684a6d6f8614
Sridhar Sarnobat <ssarnobat@control4.com>
2019-02-17 22:13:50 -0800

    Minor changes (untested)

---
+++ mwkSnippetCommands.awk
@@ -0,0 +1,10 @@
+{
+	if (NF == 3) {
+		if ($(2) > 100) {
+			print $2,$1,$3 
+		}
+	}
+	else { 
+		print "Number of fields: " NF ": " $0| "cat 1>&2" 
+	}
+}
---
+++ run_cmd_gen.sh
@@ -0,0 +1,1 @@
+cat ~/github/mahout/report_full_3_stopwords_filtered.txt | awk -f ~/github/mahout/mwkSnippetCommands.awk 2>/dev/null  | perl -pe 's{[0-9]{3} (.*).mwk: (.*)}{mv \$(grep -nl "\\b$2" *) $1/}g'
