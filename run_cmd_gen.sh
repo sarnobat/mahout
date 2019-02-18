e7f1ec8253ad075f35856ae225ae348804a530db
Sridhar Sarnobat <ssarnobat@control4.com>
2019-02-17 22:36:02 -0800

    Minor changes (untested)

--- run_cmd_gen.sh
+++ run_cmd_gen.sh
@@ -1,1 +1,7 @@
-cat ~/github/mahout/report_full_3_stopwords_filtered.txt | awk -f ~/github/mahout/mwkSnippetCommands.awk 2>/dev/null  | perl -pe 's{[0-9]{3} (.*).mwk: (.*)}{mv \$(grep -nl "\\b$2" *) $1/}g'
+# Forget about using Mahout. Do it manually until you have more training data
+
+# doesn't work well
+#cat ~/github/mahout/report_full_3_stopwords_filtered.txt | awk -f ~/github/mahout/mwkSnippetCommands.awk 2>/dev/null  | perl -pe 's{[0-9]{3} (.*).mwk: (.*)}{mv \$(grep -nl "\\b$2" *) $1/}g'
+
+# doesn't work well
+#cat ~/github/mahout/report_mwk_snpts.txt | awk -f ~/github/mahout/mwkSnippetCommands.awk 2>/dev/null  | perl -pe 's{[0-9]{2} (.*): (.*)}{mv \$(grep -nl "\\b$2" *) $1/}g'
